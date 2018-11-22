6b61638fcf2c7db78a525d603b812ae7b94aefe7
JKGu <jgu8@u.rochester.edu>
2018-11-21 19:25:48 -0500

    Initial Commit

---
+++ CSC173PJT4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSC173PJT4
+//
+//  Created by Junkang Gu on 11/21/18.
+//  Copyright © 2018 Junkang Gu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
